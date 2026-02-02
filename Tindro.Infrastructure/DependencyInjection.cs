7e9060fb9307d94d7ca4f76ca40327a3338bd80b
Bhupendra Singh Koranga <bhupendra.singh@brainnetic.com>
2026-02-02 16:37:51 +0530

    Fix Render DB connections23

--- Tindro.Infrastructure/DependencyInjection.cs
+++ Tindro.Infrastructure/DependencyInjection.cs
@@ -36,10 +36,12 @@ namespace Tindro.Infrastructure
             // ------------------------
             // DATABASES
             // ------------------------
-          var commandConn =
-    "Host=dpg-d6062aqqcgvc73a6v69g-a;Port=5432;Database=tindro_db;Username=tindro_db_user;Password=EY6FVwBXaQP2Wq8xJhdIei2lt7h2bUju";
+     var commandConn =
+    config["ConnectionStrings:CommandDb"];
 
-var queryConn = commandConn;
+var queryConn =
+    config["ConnectionStrings:QueryDb"];
+
 
 services.AddDbContext<CommandDbContext>(options =>
     options.UseNpgsql(commandConn));
